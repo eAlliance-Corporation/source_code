b33d0e012f746be1527359b4012f5f7155f8af9f
MakaSravani <139784803+MakaSravani@users.noreply.github.com>
2023-10-11 18:39:39 +0530

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -15,7 +15,7 @@ const EXAMPLES: ExampleModel[] = [
     { text: "How many locations does ALG have?", value: "How many locations does ALG have?" },
     { text: "What challenges does ALG Worldwide face ?", value: "What challenges does ALG Worldwide face ?" },
     { text: "How do I get set up with ALG?", value: "How do I get set up with ALG?" },
-    { text: "Difference between Declared Value and Shipping Insurance", value: "Difference between Declared Value and Shipping Insurance" },
+    { text: "Difference between Declared Value and Shipping Insurance?", value: "Difference between Declared Value and Shipping Insurance?" },
     { text: "Do they offer 24/7 assistance for customers?", value: "Do they offer 24/7 assistance for customers?" }
 ];
 
